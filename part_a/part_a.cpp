60366efdf57f79c65e9e34d4c706c80c2ad30f01
Akin Elden <akin.elden@boun.edu.tr>
2021-12-13 20:37:09 +0300

    Parent and child functions are seperated

--- part_a/part_a.cpp
+++ part_a/part_a.cpp
@@ -17,6 +17,123 @@ using namespace std;
 // also define the buffer size to be used while reading from pipe
 #define BUFFER_SIZE 1024
 
+// actions taken by the child(blackbox) process
+int child_process(int inputPipe[2], int outputPipe[2], int errorPipe[2], string &blackbox)
+{
+    // bind read end of the inputPipe to stdin, exit if an error occurs
+    if (dup2(inputPipe[READ_END], STDIN_FILENO) < 0)
+        return -1;
+
+    // bind write end of the outputPipe to stdout, exit if an error occurs
+    if (dup2(outputPipe[WRITE_END], STDOUT_FILENO) < 0)
+        return -1;
+
+    // bind write end of the errorPipe to stderr, exit if an error occurs
+    if (dup2(errorPipe[WRITE_END], STDERR_FILENO) < 0)
+        return -1;
+
+    // close the unused ends of pipes
+    close(inputPipe[WRITE_END]);
+    close(outputPipe[READ_END]);
+    close(errorPipe[READ_END]);
+
+    // binding is completed, load the blackbox and let it do the magic
+    if (execl(blackbox.c_str(), blackbox.c_str(), NULL) < 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+// actions taken by parent process
+int parent_process(int inputPipe[2], int outputPipe[2], int errorPipe[2], string &blackbox, string &outputPath)
+{
+    // close the read end of input and write end of output/error pipes
+    close(inputPipe[READ_END]);
+    close(outputPipe[WRITE_END]);
+    close(errorPipe[WRITE_END]);
+
+    // read two integers from stdin convert them to single line of input
+    int first, second;
+    cin >> first;
+    cin >> second;
+    string input = to_string(first) + " " + to_string(second) + "\n";
+
+    // try to write to inputPipe, if cannot write return -1
+    if (write(inputPipe[WRITE_END], input.c_str(), input.size() + 1) <= 0)
+        return -1;
+
+    // open the output file
+    fstream outputFile;
+    outputFile.open(outputPath, ios::app);
+
+    // wait for child process to finish its job and get the return status
+    int status;
+    wait(&status);
+
+    // if the exit status of child is not 0, then an error occurred
+    bool normalExit = false;
+    if (WIFEXITED(status))
+    {
+        int exitStatus = WEXITSTATUS(status);
+        normalExit = exitStatus == 0;
+    }
+
+    if (normalExit) // if child terminated normally, read from outputPipe (stdout)
+    {
+        char buff[BUFFER_SIZE]; // the buffer to read from pipe
+        // try to read the result from outputPipe
+        if (read(outputPipe[READ_END], buff, sizeof(buff)) > 0)
+        {
+            // if successfully read, get the integer value from buff
+            int result = stoi(string(buff));
+            // first write SUCCESS and then result to outputFile
+            outputFile << "SUCCESS:" << endl;
+            outputFile << result << endl;
+        }
+        else // if couldn't read, it means something is wrong, return -1
+            return -1;
+    }
+    else // if not a normal exit, then read from errorPipe (stderr)
+    {
+        // use a string and accumulate pipe data inside it
+        string errorMsg = "";
+        while (true)
+        {
+            // initialize a char array and try to read from errorPipe
+            char msg[BUFFER_SIZE];
+            int nRead; // number of bytes read from pipe
+            nRead = read(errorPipe[READ_END], msg, BUFFER_SIZE);
+            if (nRead > 0) // if some content is read, add it to errorMsg
+            {
+                errorMsg += string(msg);
+                if (nRead < BUFFER_SIZE) // it means all content is read from pipe
+                    break;
+            }
+            else // otherwise complete content is read, break the loop
+                break;
+        }
+
+        if (errorMsg.size() > 0) // if some message is read, write it to outputFile
+        {
+            outputFile << "FAIL:" << endl;
+            outputFile << errorMsg;
+            if (errorMsg.back() != '\n')
+                outputFile << endl;
+        }
+        else // if no data is read, something is wrong return -1
+            return -1;
+    }
+
+    // close the output file, pipes and return
+    outputFile.close();
+    close(inputPipe[WRITE_END]);
+    close(outputPipe[READ_END]);
+    close(errorPipe[READ_END]);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     // 3 parameters are required: current executable, blackbox path, output file path
@@ -39,112 +156,10 @@ int main(int argc, char *argv[])
     int pid = fork();
     if (pid == 0) // child process actions
     {
-        // bind read end of the inputPipe to stdin, exit if an error occurs
-        if (dup2(inputPipe[READ_END], STDIN_FILENO) < 0)
-            return -1;
-
-        // bind write end of the outputPipe to stdout, exit if an error occurs
-        if (dup2(outputPipe[WRITE_END], STDOUT_FILENO) < 0)
-            return -1;
-
-        // bind write end of the errorPipe to stderr, exit if an error occurs
-        if (dup2(errorPipe[WRITE_END], STDERR_FILENO) < 0)
-            return -1;
-
-        // close the unused ends of pipes
-        close(inputPipe[WRITE_END]);
-        close(outputPipe[READ_END]);
-        close(errorPipe[READ_END]);
-
-        // binding is completed, load the blackbox and let it do the magic
-        if (execl(blackbox.c_str(), blackbox.c_str(), NULL) < 0)
-        {
-            return -1;
-        }
+        return child_process(inputPipe, outputPipe, errorPipe, blackbox);
     }
     else // parent process actions
     {
-        // close the read end of input and write end of output/error pipes
-        close(inputPipe[READ_END]);
-        close(outputPipe[WRITE_END]);
-        close(errorPipe[WRITE_END]);
-
-        // read two integers from stdin convert them to single line of input
-        int first, second;
-        cin >> first;
-        cin >> second;
-        string input = to_string(first) + " " + to_string(second) + "\n";
-
-        // try to write to inputPipe, if cannot write return -1
-        if (write(inputPipe[WRITE_END], input.c_str(), input.size() + 1) <= 0)
-            return -1;
-
-        // open the output file
-        fstream outputFile;
-        outputFile.open(outputPath, ios::app);
-
-        // wait for child process to finish its job and get the return status
-        int status;
-        wait(&status);
-
-        // if the exit status of child is not 0, then an error occurred
-        bool normalExit = false;
-        if (WIFEXITED(status))
-        {
-            int exitStatus = WEXITSTATUS(status);
-            normalExit = exitStatus == 0;
-        }
-
-        if (normalExit) // if child terminated normally, read from outputPipe (stdout)
-        {
-            char buff[BUFFER_SIZE]; // the buffer to read from pipe
-            // try to read the result from outputPipe
-            if (read(outputPipe[READ_END], buff, sizeof(buff)) > 0)
-            {
-                // if successfully read, get the integer value from buff
-                int result = stoi(string(buff));
-                // first write SUCCESS and then result to outputFile
-                outputFile << "SUCCESS:" << endl;
-                outputFile << result << endl;
-            }
-            else // if couldn't read, it means something is wrong, return -1
-                return -1;
-        }
-        else // if not a normal exit, then read from errorPipe (stderr)
-        {
-            // use a string and accumulate pipe data inside it
-            string errorMsg = "";
-            while (true)
-            {
-                // initialize a char array and try to read from errorPipe
-                char msg[BUFFER_SIZE];
-                int nRead; // number of bytes read from pipe
-                nRead = read(errorPipe[READ_END], msg, BUFFER_SIZE);
-                if (nRead > 0) // if some content is read, add it to errorMsg
-                {
-                    errorMsg += string(msg);
-                    if (nRead < BUFFER_SIZE) // it means all content is read from pipe
-                        break;
-                }
-                else // otherwise complete content is read, break the loop
-                    break;
-            }
-
-            if (errorMsg.size() > 0) // if some message is read, write it to outputFile
-            {
-                outputFile << "FAIL:" << endl;
-                outputFile << errorMsg << endl;
-            }
-            else // if no data is read, something is wrong return -1
-                return -1;
-        }
-
-        // close the output file, pipes and return
-        outputFile.close();
-        close(inputPipe[WRITE_END]);
-        close(outputPipe[READ_END]);
-        close(errorPipe[READ_END]);
+        return parent_process(inputPipe, outputPipe, errorPipe, blackbox, outputPath);
     }
-
-    return 0;
 }
